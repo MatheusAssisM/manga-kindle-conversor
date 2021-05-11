4a8192420fecb9c88277b98d28dd4aaf4896d03e
Matheus de Assis <matheusassismelo@gmail.com>
2021-05-11 01:56:34 -0300

    Juntando pdf em uma pagina e removendo imagens

--- src/conversor.py
+++ src/conversor.py
@@ -1,5 +1,7 @@
 from PIL import Image
 from os import  listdir, remove, mkdir, path
+from PyPDF2 import PdfFileMerger, PdfFileReader
+
 
 def converter(arquivo, pasta_manga):
     """
@@ -18,11 +20,11 @@ def separar_capitulos(paginas):
     separa todos os capitulos do manga
     """
     print("Separando capitulos...")
-    num_capitulos = set(list(map(lambda x: int(x.split("_")[0]), paginas)))
+    num_capitulos = set(list(map(lambda x: x.split("_")[0], paginas)))
     capitulos = dict(map(lambda x: (str(x), [] ), num_capitulos))
 
     for pagina in paginas:
-        num_pagina = int(pagina.split("_")[0])
+        num_pagina = pagina.split("_")[0]
         if num_pagina in num_capitulos:
             capitulos[str(num_pagina)].append(pagina)
     return capitulos
@@ -34,6 +36,22 @@ def criar_pastas_capitulos(capitulos, pasta_manga):
     print("Criando pastas para os capitulos...")
     list(map(lambda x: mkdir(f"{pasta_manga}/{str(x)}/"), capitulos.keys()))
 
+def remover_imagens(pasta_manga):
+    print("Removendo imagens")
+    list(map(
+        lambda x: remove(f"{pasta_manga}/{x}"), 
+        list(filter(lambda x: True if x.endswith(".jpg") else False, listdir(pasta_manga)))
+        ))
+
+def juntar_paginas_pdf(pasta_manga, /):
+    print("Juntando páginas...")
+    mergerPDF = PdfFileMerger()
+    for folder in listdir(pasta_manga):
+        for pdf in listdir(f"{pasta_manga}/{folder}"):
+            mergerPDF.append(PdfFileReader(f"{pasta_manga}/{folder}/{pdf}"))
+    mergerPDF.write(f"{pasta_manga}/{pasta_manga}.pdf")
+
+
 def run(pasta_manga):
     #busca todas as imagens dentro da pasta informada
     print("Buscando imagens...")
@@ -41,9 +59,10 @@ def run(pasta_manga):
 
     #coloca tudo na ordem correta
     print("Organizando...")
-    imagens.sort(key=lambda x: int(x.split("_")[1].split(".")[0]))
-    imagens.sort(key=lambda x: int(x.split("_")[0]))
-
+    imagens.sort(key=lambda x: x.split("_")[1].split(".")[0])
+    imagens.sort(key=lambda x: x.split("_")[0])
     capitulos = separar_capitulos(imagens)
     criar_pastas_capitulos(capitulos, pasta_manga)
     converter(capitulos, pasta_manga)
+    remover_imagens(pasta_manga)
+    juntar_paginas_pdf(pasta_manga)
--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
 import conversor
 
-pasta_manga = input("Insira o caminho do manga") #unica variável que precisa ser alterada
+pasta_manga = "./Jujutsu Kaisen" #unica variável que precisa ser alterada
 conversor.run(pasta_manga)
